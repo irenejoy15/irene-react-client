e8e9358c7f59943368e9d47580c3306fb0bd0bca
Cefrian Yves Trinchera <cefrian.trinchera@SANPABLO.LOCAL>
2025-07-07 10:25:58 +0800

    SETUP INDEX JS

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import HomeLayout from './pages/HomeLayout';
+import {
+  HomeLayout,
+  Register,
+  Login,
+  Error,
+  Landing,
+  DashboardLayout
+} from './pages';
 
 const router = createBrowserRouter([
   {
     path:'/',
     element:<HomeLayout/>
-  },{
-    path:'/about',
-    element:(
-      <div>
-        <h1>ABOUT PAGE</h1>
-      </div>
-    )
+  },
+  {
+    path:'/register',
+    element:<Register/>
+  },
+  {
+    path:'/login',
+    element:<Login/>
+  }
+  ,
+  {
+    path:'/dashboard',
+    element:<DashboardLayout/>
   }
 ]);
 
