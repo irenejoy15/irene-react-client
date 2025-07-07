94697b15a48392b63c0c8e58b174d9a475d50067
Cefrian Yves Trinchera <cefrian.trinchera@SANPABLO.LOCAL>
2025-07-07 14:38:33 +0800

    FormRow Component

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,29 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
-import { Logo } from '../components';
+import { FormRow,Logo } from '../components';
 
 const Register = () => {
   return <Wrapper>
   <form className='form'>
     <Logo/>
     <h4>Register</h4>
-    <div className="form-row">
-      <label htmlFor="name" className='form-label'>
-        name
-      </label>
-      <input type="text" id="name" name="name" className='form-input' defaultValue="IRENE" />
-      <button type='submit' className='btn btn-block'>
-        SUBMIT
-      </button>
+    
+    <FormRow type="text" name="firstName" labelText='First Name' defaultValue='irene'/>
+    <FormRow type="text" name="lastName" labelText='Last Name' defaultValue='joy'/>
+    <FormRow type="text" name="location" labelText='Location'  defaultValue='earth'/>
+    <FormRow type="email" name="email" labelText='Email'  defaultValue='irene@gmail.com'/>
+    <FormRow type="password" name="password" labelText='Password' defaultValue='irene143143'/>
 
-      <p>
-        Already a Member
-        <Link to='/login' className='member-btn'>
-          Login
-        </Link>
-      </p>
-    </div>
+    <button type='submit' className='btn btn-block'>
+      SUBMIT
+    </button>
+
+    <p>
+      Already a Member
+      <Link to='/login' className='member-btn'>
+        Login
+      </Link>
+    </p>
+  
   </form>
   </Wrapper>
 }
