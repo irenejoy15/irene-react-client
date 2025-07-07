04139b33be2979cd261bbccbfb29c26a12283a74
Cefrian Yves Trinchera <cefrian.trinchera@SANPABLO.LOCAL>
2025-07-07 10:04:50 +0800

    INITIAL SETUP

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<h1>jhome</h1>
+  },{
+    path:'/about',
+    element:(
+      <div>
+        <h1>ABOUT PAGE</h1>
+      </div>
+    )
+  }
+]);
 
 const App = () => {
-  return (
-    <h1>
-      Jobify App
-    </h1>
-  )
+  return <RouterProvider router = {router}/>
 }
 
 export default App
