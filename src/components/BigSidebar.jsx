0232b5e01d0c1e5deb4bd2b4183d94f7d5022705
Cefrian Yves Trinchera <cefrian.trinchera@SANPABLO.LOCAL>
2025-07-09 09:59:56 +0800

    BigSideBar

--- src/components/BigSidebar.jsx
+++ src/components/BigSidebar.jsx
@@ -1,9 +1,19 @@
 import Wrapper from "../assets/wrappers/BigSidebar"
-
+import Navlinks from "./Navlinks"
+import Logo from "./Logo"
+import { useDashboardContext } from "../pages/DashboardLayout"
 const BigSidebar = () => {
+  const {showSideBar} = useDashboardContext();
   return (
     <Wrapper>
-        BIG SIDEBAR
+        <div className={showSideBar?'sidebar-container':'sidebar-container  show-sidebar'}>
+          <div className="content">
+            <header>
+              <Logo/>
+            </header>
+            <Navlinks/>
+          </div>
+        </div>
     </Wrapper>
   )
 }
--- src/components/Navlinks.jsx
+++ src/components/Navlinks.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import links from '../utils/links'
 import { useDashboardContext } from '../pages/DashboardLayout'
 import { NavLink } from 'react-router-dom'
-const Navlinks = () => {
+const Navlinks = (isBigSidebar) => {
     const {toggleSidebar,user} = useDashboardContext();
     return (
         <div className="nav-links">
             {links.map((link)=>{
             const {text,path,icon} = link
-            return <NavLink to={path} key={text} className='nav-link' onClick={toggleSidebar}>
+            return <NavLink to={path} key={text} className='nav-link' onClick={isBigSidebar ? null : toggleSidebar}>
                 <span className='icon'>{icon}</span>
                 {text}
             </NavLink>
