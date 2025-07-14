7fdb3ea3558d7bf6ce08fc777c586b660a4c9cdd
Cefrian Yves Trinchera <cefrian.trinchera@SANPABLO.LOCAL>
2025-07-14 10:55:54 +0800

    creation of custom fetch

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import customFetch from './utils/customFetch.js'
 
-fetch('/api/v1/test')
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+const response = await customFetch.get('/test');
+
+console.log(response);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
