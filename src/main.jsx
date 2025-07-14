9293eca9e6f13f6e0e0b6e86143f56ccf6757cb9
Cefrian Yves Trinchera <cefrian.trinchera@SANPABLO.LOCAL>
2025-07-14 10:57:12 +0800

    REMOVE MAIN TESTING ONLY

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import customFetch from './utils/customFetch.js'
+// import customFetch from './utils/customFetch.js'
 
-const response = await customFetch.get('/test');
+// const response = await customFetch.get('/test');
 
-console.log(response);
+// console.log(response);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
