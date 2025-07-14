f0fb80e77e916d5fe8ecbda04937759f2c943a7b
Cefrian Yves Trinchera <cefrian.trinchera@SANPABLO.LOCAL>
2025-07-14 16:01:57 +0800

    React TOasttify

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-// import customFetch from './utils/customFetch.js'
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+import ReactDOM from 'react-dom/client'
 
-// const response = await customFetch.get('/test');
-
-// console.log(response);
-
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    <ToastContainer position='top-center' />
+  </StrictMode>
+);
