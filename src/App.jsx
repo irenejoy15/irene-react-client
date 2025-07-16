f72fe33de42a56e781c999b70899553896912ed1
Cefrian Yves Trinchera <cefrian.trinchera@SANPABLO.LOCAL>
2025-07-16 14:40:37 +0800

    EDIT JOB LOADER

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,8 @@ import { action as loginAction} from './pages/Login';
 import { loader as loaderAction} from './pages/DashboardLayout';
 import { action as addJobAction} from './pages/AddJob';
 import { loader as allJobsLoader} from './pages/AllJobs';
-
+import { loader as editJobLoader} from './pages/EditJob';
+import { action as editJobAction} from './pages/EditJob';
 export const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
   document.body.classList.toggle('dark-theme',isDarkTheme);
@@ -80,6 +81,8 @@ const router = createBrowserRouter([
           {
             path:'edit-job/:id',
             element: <EditJob/>,
+            loader: editJobLoader,
+            action: editJobAction
           }
         ]
       }
--- src/pages/EditJob.jsx
+++ src/pages/EditJob.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import { FormRow, FormRowSelect } from '../components';
+import Wrapper from '../assets/wrappers/DashboardFormPage';
+import { useLoaderData, useParams } from 'react-router-dom';
+import { JOB_STATUS, JOB_TYPE } from '../../../utils/constant';
+import { Form, useNavigation, redirect } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import customFetch from '../utils/customFetch';
+
+export const loader = async ({params}) => {
+  try{
+    const {data} = await customFetch.get(`/jobs/${params.id}`);
+    return data;
+  }catch(error){
+    toast.error(error?.response?.data?.msg);
+    return redirect('dashboard/all-jobs');
+  }
+};
+
+export const action = async()=>{
+  return null;
+};
 
 const EditJob = () => {
+  // const params = useParams();
+  // console.log(params);
   return (
     <h1>EDIT JOB</h1>
   )
