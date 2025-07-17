18af2c40bcca131da8c01c162610fb97d59d84ba
Cefrian Yves Trinchera <cefrian.trinchera@SANPABLO.LOCAL>
2025-07-17 11:19:25 +0800

    PAGINATION

--- src/components/JobContainer.jsx
+++ src/components/JobContainer.jsx
@@ -2,21 +2,24 @@ import React from 'react'
 import Job from './Job'
 import Wrapper from '../assets/wrappers/JobsContainer'
 import { useAllJobsContext } from '../pages/AllJobs'
+import PageBtnContainer from './PageBtnContainer'
 
 const JobContainer = () => {
     const {data} = useAllJobsContext();
-    const {jobs} = data 
+    const {jobs,totalJobs,numOfPages} = data 
     if(jobs.length ===0){
         return <Wrapper>
             <h2>No JOBS TO DISPLAY</h2>
         </Wrapper>
     }
     return <Wrapper>
+        <h5>{totalJobs} job{totalJobs > 1 && 's'}</h5>
         <div className="jobs">
             {jobs.map((job)=>{
                 return <Job key={job._id} {...job}/>
             })}
         </div>
+        {numOfPages > 1 && <PageBtnContainer/>}
     </Wrapper>
 }
 
